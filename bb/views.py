3953a06b42b883283ca3cd9809ae7c7d331b3284
WhiteLedAnn <nonameyetithink@mail.ru>
2019-11-09 17:45:56 +0300

    urls

--- bb/views.py
+++ bb/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+def home(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Отрисовка HTML-шаблона home.html с данными внутри переменной контекста context
+    return render(request, 'home.html', context = {})
+
+def products_list(request):
+    return render(request, 'products/products_list.html', {})
+
 # Create your views here.
